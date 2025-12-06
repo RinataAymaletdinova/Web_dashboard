d5a07193d8d376e046cc7b7402ebfa5ff3401587
RinataAymaletdinova <125034890+RinataAymaletdinova@users.noreply.github.com>
2025-12-06 23:43:33 +0300

    Update CatsWidget.js

--- js/CatsWidget.js
+++ js/CatsWidget.js
@@ -3,23 +3,42 @@ import UIComponent from './UIComponent.js';
 export default class CatsWidget extends UIComponent {
     constructor() {
         super('cats', 'Факты о котах', '🐱');
-        this.apiUrl = 'https://catfact.ninja/fact';
+        this.apiUrl = 'https://meowfacts.herokuapp.com/';
     }
 
     async load() {
         this.showLoading();
         
         try {
-            const response = await fetch(this.apiUrl);
+            const response = await fetch(this.apiUrl, {
+                headers: {
+                    'Accept': 'application/json',
+                }
+            });
+            
+            if (!response.ok) {
+                throw new Error(`Ошибка HTTP: ${response.status}`);
+            }
+            
             const data = await response.json();
             
-            this.updateContent(`
-                <div class="cats-fact">
-                    <p>${data.fact}</p>
-                </div>
-            `);
+            // API meowfacts возвращает {data: ["факт"]}
+            if (data.data && data.data.length > 0) {
+                this.updateContent(`
+                    <div class="cats-fact">
+                        <p>${data.data[0]}</p>
+                    </div>
+                `);
+            } else {
+                this.showError('API не вернул факт');
+            }
+            
         } catch (error) {
-            this.showError('Коты сейчас спят, попробуй позже');
+            this.showError(`Не удалось загрузить факт о котах: ${error.message}`);
         }
+    }
+
+    retryLoad() {
+        this.load();
     }
 }
